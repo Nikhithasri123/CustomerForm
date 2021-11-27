d225ed90955c0029bb89e5017ec2e1ba8aa475cb
Nikhithasri123 <65812940+Nikhithasri123@users.noreply.github.com>
2021-11-27 15:02:28 +0530

    index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+let product=["samsung","hp","dell","lg","haier","oppo"];
+
+document.getElementById('demo1').innerHTML=product.shift();
+document.getElementById('demo2').innerHTML=product;
+
+ 
+
+ 
+ 
